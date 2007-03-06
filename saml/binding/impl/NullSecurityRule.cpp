4ad805325a28a192f7000c2a6bbfa2343adad2cb
Scott Cantor <cantor.2@osu.edu>
2007-03-06 17:53:00 +0000

    Null security rule for disabling security checks.

---
+++ saml/binding/impl/NullSecurityRule.cpp
@@ -0,0 +1,56 @@
+/*
+ *  Copyright 2001-2007 Internet2
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * NullSecurityRule.cpp
+ * 
+ * SecurityPolicyRule that "disables" security. 
+ */
+
+#include "internal.h"
+#include "binding/SecurityPolicyRule.h"
+
+#include <log4cpp/Category.hh>
+
+using namespace opensaml;
+using namespace xmltooling;
+using namespace log4cpp;
+using namespace std;
+
+namespace opensaml {
+    class SAML_DLLLOCAL NullSecurityRule : public SecurityPolicyRule
+    {
+    public:
+        NullSecurityRule(const DOMElement* e) : m_log(Category::getInstance(SAML_LOGCAT".SecurityPolicyRule.NullSecurity")) {}
+        virtual ~NullSecurityRule() {}
+        
+        void evaluate(const XMLObject& message, const GenericRequest* request, SecurityPolicy& policy) const;
+
+    private:
+        Category& m_log;
+    };
+
+    SecurityPolicyRule* SAML_DLLLOCAL NullSecurityRuleFactory(const DOMElement* const & e)
+    {
+        return new NullSecurityRule(e);
+    }
+};
+
+void NullSecurityRule::evaluate(const XMLObject& message, const GenericRequest* request, SecurityPolicy& policy) const
+{
+    m_log.warn("security enforced using NULL policy rule, be sure you know what you're doing");
+    policy.setSecure(true);
+}
