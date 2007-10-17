58624bfd86fc91c48f7c4f31c1f1736d924b1911
Scott Cantor <cantor.2@osu.edu>
2007-10-17 06:20:51 +0000

    Convert usage bits to a mask.

--- saml/saml2/metadata/MetadataCredentialCriteria.h
+++ saml/saml2/metadata/MetadataCredentialCriteria.h
@@ -64,10 +64,10 @@ namespace opensaml {
                 const MetadataCredentialContext* context = dynamic_cast<const MetadataCredentialContext*>(credential.getCredentalContext());
                 if (context) {
                     // Check for a usage mismatch.
-                    if ((getUsage()==xmltooling::Credential::SIGNING_CREDENTIAL || getUsage()==xmltooling::Credential::TLS_CREDENTIAL) &&
+                    if ((getUsage() | (xmltooling::Credential::SIGNING_CREDENTIAL & xmltooling::Credential::TLS_CREDENTIAL)) &&
                             XMLString::equals(context->getKeyDescriptor().getUse(),KeyDescriptor::KEYTYPE_ENCRYPTION))
                         return false;
-                    else if (getUsage()==xmltooling::Credential::ENCRYPTION_CREDENTIAL &&
+                    else if ((getUsage() | xmltooling::Credential::ENCRYPTION_CREDENTIAL) &&
                             XMLString::equals(context->getKeyDescriptor().getUse(),KeyDescriptor::KEYTYPE_SIGNING))
                         return false;
                 }
