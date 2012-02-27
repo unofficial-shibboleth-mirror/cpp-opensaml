b4bc73359af13f7b80e986e90647330d951e747d
Scott Cantor <cantor.2@osu.edu>
2012-02-27 00:23:00 +0000

    Fix a typo in constructor

--- saml/saml2/metadata/impl/BlacklistMetadataFilter.cpp
+++ saml/saml2/metadata/impl/BlacklistMetadataFilter.cpp
@@ -64,14 +64,14 @@ namespace opensaml {
         }
 
         static const XMLCh Exclude[] = UNICODE_LITERAL_7(E,x,c,l,u,d,e);
-        static const XMLCh matcher[] = UNICODE_LITERAL_7(m,a,t,c,h,e,r);
+        static const XMLCh _matcher[] = UNICODE_LITERAL_7(m,a,t,c,h,e,r);
     };
 };
 
 
 BlacklistMetadataFilter::BlacklistMetadataFilter(const DOMElement* e)
 {
-    string matcher(XMLHelper::getAttrString(e, nullptr, matcher));
+    string matcher(XMLHelper::getAttrString(e, nullptr, _matcher));
     if (!matcher.empty())
         m_matcher.reset(SAMLConfig::getConfig().EntityMatcherManager.newPlugin(matcher.c_str(), e));
 
