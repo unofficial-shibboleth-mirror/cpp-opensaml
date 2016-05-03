7efda6ea0b243078dbafac469b25b99d821d06c6
Scott Cantor <cantor.2@osu.edu>
2016-05-03 11:28:48 -0400

    Bump version to 2.6.0 and adjust soname and DLL name.

--- config_win32.h
+++ config_win32.h
@@ -87,13 +87,13 @@
 #define PACKAGE_NAME "opensaml"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "opensaml 2.5.5"
+#define PACKAGE_STRING "opensaml 2.6.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "opensaml"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.5.5"
+#define PACKAGE_VERSION "2.6.0"
 
 /* Define to the necessary symbol if this constant uses a non-standard name on
    your system. */
@@ -106,7 +106,7 @@
 /* #undef TM_IN_SYS_TIME */
 
 /* Version number of package */
-#define VERSION "2.5.5"
+#define VERSION "2.6.0"
 
 /* Define to empty if `const' does not conform to ANSI C. */
 /* #undef const */
